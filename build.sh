04725670570d1b8bdbf61b897ab65cdadc79cccc
Jorge Ulises Useche Cuellar <naturalmentejorge@gmail.com>
2018-06-26 15:01:43 -0500

    Add decript secrets xtf2json

--- build.sh
+++ build.sh
@@ -1,3 +1,16 @@
 #!/bin/bash -eux
-gradle build
+
+# rationale: set a default DEPLOY_SECRETS
+if [ -z "$JENKINS_DEPLOY" ]; then
+  JENKINS_DEPLOY='no'
+fi
+
+if [ "$JENKINS_DEPLOY" == "yes" ]
+then
+  # Opcional, configura la aplicación descifrando secretos.
+  rsaconfigcipher -P $JENKINS_SECRET_KEY src/main/resources/application.properties.rsa
+fi
+
+# Generate artifact
+gradle assemble
 # this generates build/libs/xtf2json-0.0.1-SNAPSHOT.jar
